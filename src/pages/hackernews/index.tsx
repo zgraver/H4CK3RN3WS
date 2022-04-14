7ae1a3b67f9828aae5788dffcfb004b2226e558a
Anton Popov <zurich.anton.popov@gmail.com>
2022-04-14 14:45:38 +0200

    test api response

---
+++ src/pages/hackernews/index.tsx
@@ -0,0 +1,15 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const HackerNewsPage: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Redux Toolkit</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </div>
+  );
+};
+
+export default HackerNewsPage;
