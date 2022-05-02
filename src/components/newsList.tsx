ce3bc3f68e178e3fe5dd968e9ac3d4e0e122b717
Anton Popov <zurich.anton.popov@gmail.com>
2022-05-02 06:21:23 +0200

    add filter with no functionality

--- src/components/newsList.tsx
+++ src/components/newsList.tsx
@@ -1,23 +1,40 @@
 import useSWR from "swr";
 import Link from "next/link";
+import { useState } from "react";
 
 const fetcher = (url) => fetch(url).then((res) => res.json());
+
+// Has a search input field to query articles on the server using the "query" parameter
 
 const NewsList = ({ hidden = false }) => {
   const { data, error } = useSWR(
     "https://hn.algolia.com/api/v1/search",
     fetcher
   );
+  const [search, setSearch] = useState("");
   return (
-    <div className={`news-list ${hidden ? "hidden" : ""}`}>
-      {data?.hits?.map(({ objectID: id, title, url, author, num_comments }) => (
-        <li className="news-item">
-          <Link className="news-title" href={`/hackernews/${id}`}>
-            <a>{title}</a>
-          </Link>
-        </li>
-      ))}
-    </div>
+    <>
+      <form onSubmit={(e) => e.preventDefault()}>
+        <input
+          type="text"
+          value={search}
+          placeholder="Search"
+          onChange={(e) => setSearch(e.target.value)}
+        />
+        <button type="submit">Search</button>
+      </form>
+      <div className={`news-list ${hidden ? "hidden" : ""}`}>
+        {data?.hits?.map(
+          ({ objectID: id, title, url, author, num_comments }) => (
+            <li className="news-item">
+              <Link className="news-title" href={`/hackernews/${id}`}>
+                <a>{title}</a>
+              </Link>
+            </li>
+          )
+        )}
+      </div>
+    </>
   );
 };
 
